8442b597845f3370a974ecee1ac19e6b93e7070b
Nika Darab <nikadarab@pm.me>
2021-01-25 21:51:09 -0600

    folders endpoint

--- src/app.js
+++ src/app.js
@@ -6,17 +6,24 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
-
 const app = express();
-
+// const notesRouter = require("./notes/notes-router");
+const foldersRouter = require("./folders/folders-router");
+const { CLIENT_ORIGIN } = require("./config");
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
 
+// app.use("/notes", notesRouter);
+app.use("/folders", foldersRouter);
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Hello, world!!!");
 });
 
 app.use(function errorHandler(error, req, res, next) {
