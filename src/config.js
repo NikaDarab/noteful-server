5e8265c6758d8b39c230b0eec69f59d5a21fd6fe
Nika Darab <nikadarab@pm.me>
2021-01-26 20:21:53 -0600

    prod ready

--- src/config.js
+++ src/config.js
@@ -2,6 +2,10 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://nikadarab@localhost/noteful",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://nikadarab@localhost/noteful",
   API_TOKEN: process.env.API_TOKEN || "0e0a6da6-d88b-49d3-ab9a-b28f75efaf2b",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://nikadarab@localhost/noteful-test",
 };
--- src/server.js
+++ src/server.js
@@ -2,14 +2,15 @@
 /* eslint-disable quotes */
 const knex = require("knex");
 const app = require("./app");
-const { PORT, DB_URL } = require("./config");
+const { PORT, DATABASE_URL } = require("./config");
+
 const pg = require("pg");
 
 pg.defaults.ssl = process.env.NODE_ENV === "production";
 
 const db = knex({
   client: "pg",
-  connection: DB_URL,
+  connection: DATABASE_URL,
 });
 app.set("db", db);
 app.listen(PORT, () => {
