787c7a4d247470d6199c8bccc7636e0c7bad4c68
Nika Darab <nikadarab@pm.me>
2021-01-27 13:22:42 -0600

    pq update

--- src/validateBearerToken.js
+++ src/validateBearerToken.js
@@ -2,8 +2,10 @@
 function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
-
+  console.log(apiToken);
+  console.log(authToken);
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    console.log(authToken.split(" ")[1]);
     return res.status(401).json({ error: "Unauthorized request" });
   }
   //   move to the next middleware
